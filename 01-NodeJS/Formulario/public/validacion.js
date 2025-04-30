a7e285c2d962d0b70167495ab642085ebea5d5de
PughTiziano <tizipugh079@gmail.com>
2025-04-30 19:33:07 -0300

    30/4 pt3

---
+++ 01-NodeJS/Formulario/public/validacion.js
@@ -0,0 +1,46 @@
+document.getElementById('registrar').addEventListener('submit', registrar);
+
+function registrar(event)
+{
+    let inputUsuario = document.getElementById('usuarioInput');
+    let inputEmail = document.getElementById('emailInput');
+    let inputContra = document.getElementById('contraInput');
+    let validarContra = document.getElementById('contraInput2');
+    let valido = true;
+
+    if(inputUsuario.value.length === 0 || inputEmail.value.length === 0 || inputContra.value.length === 0 || validarContra.value.length === 0)
+    {
+        alert('Rellenar todos los Campos');
+        event.preventDefault();
+        valido = false;
+    }
+    else if(!(inputContra.value == validarContra.value))
+    {
+        alert('Contraseñas distintas');
+        event.preventDefault();
+        valido = false;
+    }
+    else if(!verificarEmail(inputEmail.value))
+    {
+        alert('Email Invalido');
+        event.preventDefault();
+        valido = false;
+    }
+
+    if(valido == true)
+    {
+        alert('Registro Exitoso');
+    }
+
+}
+
+function verificarEmail(email)
+{
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    return emailRegex.test(email)
+}
+
+function login(event)
+{
+
+}
--- 01-NodeJS/Formulario/server.js
+++ 01-NodeJS/Formulario/server.js
@@ -2,9 +2,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = 8081;
+const usuarios = [];
+
+app.use(express.urlencoded({extended : true}));
 
 //Le indicamos a app que utilize la carpeta de public
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/enviar', (req, res) =>{
+    
+    const usuario = {
+        usr : req.body.usr,
+        pass : req.body.pass
+    }
+    usuarios.push(usuario);
+    console.log(usuario);
+    res.send('Usuario agregado correctamente, <a href="/"> Volver </a>');
+});
+
 
 //Creamos el servidor y le agregamos cada ruta que hace falta, uno para el index y uno por pagina web
 app.get('/', (req, res) => {
@@ -12,7 +28,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/usuarios', (req, res) => {
+    let lista = '<style> *{text-align: center;} </style>';
+    lista += '<h1> Listado Usuarios </h1><ul>';
+    usuarios.forEach(p => {
+        lista += '<li>' + p.usr + ' ' + p.pass + 'A</li>';
+    });
+    lista += '</ul><a href="/"> Volver </a>';
+    res.send(lista);
+});
+
 //Iniciando el servidor
-app.listen(8081, '127.0.0.1', () => {
-    console.log('Listening on 127.0.0.1:8081');
+app.listen(port, '127.0.0.1', () => {
+    console.log('Listening on 127.0.0.1:' + port);
 });
