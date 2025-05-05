ff35748311d4314a403bc312548e6de8f9fc019b
PughTiziano <tizipugh079@gmail.com>
2025-05-05 20:40:49 -0300

    ajuste

--- 02-JS/JS0/public/script.js
+++ 02-JS/JS0/public/script.js
@@ -1,11 +1,12 @@
 document.getElementById('registrar').addEventListener('submit', registrar);
 document.getElementById('hijos').addEventListener('change', mostrarHijos);
-let usuarios = 0;
+let usuarios = 0; // Cantidad de Usuarios
 
-function registrar(event)
+function registrar(event) //Funcion para registrar a los usuarios
 {
-    event.preventDefault()
+    event.preventDefault() //Previene que se reinicie la pagina
     let valido = true;
+    //Guardo todos los valores del formulario
     let nombre = document.getElementById('nombreInput').value;
     let apellido = document.getElementById('apellidoInput').value;
     let nacimiento = document.getElementById('nacimientoInput').value;
@@ -20,24 +21,29 @@ function registrar(event)
 
     if(nombre == '' || apellido == '' || nacimiento == '' || documento == '' || nacionalidad == '' || telefono == '' || email == '')
     {
+        //Se revisa que no esten en blanco, y en caso de estarlos alerta al usuario
         alert("Completar todos los campos");
         valido = false;
     }
     if(hijo.checked && cantHijos == '')
     {
+        //Revisa que el campo de la cantidad de hijos no este en blanco
         alert("Completar todos los campos");
         valido = false;
     }
     if(!verificarEmail(email))
     {
+        //Verifica que el email sea valido
         alert("Ingresar un Email valido");
         valido = false;
     }
     if(!checkTelef(telefono))
     {
+        //Verifica el numero de telefono sea valido
         alert("Ingresar un Telefono valido de la siguiente manera: 123 456 7899");
         valido = false;
     }
+    //Verifica que el nombre y el apellido sean validos
     if(verificarText(nombre))
     {
         alert("Ingresar un Nombre valido");
@@ -48,14 +54,17 @@ function registrar(event)
         alert("Ingresar un Apellido valido");
         valido = false;
     }
+    //Verifica que el documento sea valido
     if(verificarNum(documento))
     {
         alert("Ingresar un Documento valido");
         valido = false;
     }
 
+    //En caso de que todo sea valido
     if(valido)
     {
+        //Le agrega toda la informacion al div de usuarios selection
         let info = '<div class="usuario"><p style="display: inline-block;">' + nombre + ' ' + apellido +'</p>';
         info += '<button onclick="mostrarUsuarioInfo('+ usuarios +')" class="botonDesplegable">V</button><div class="usuarioInfo" id="us' + usuarios + '">';
         info += '<p>Fecha Nacimiento: ' + nacimiento + '</p>';
@@ -68,25 +77,28 @@ function registrar(event)
         else info += '<p>Hijos: No</p>';
         info += '<p>Email: ' + email + '</p></div></div>';
         document.getElementById('usuariosSection').innerHTML += info;
-        usuarios++;
-        alert("Guardado Existoso");
+        usuarios++; //Agrega un usuario mas
+        alert("Guardado Existoso"); //Le alerta al usuario que se pudo guardar
     }
 }  
 
 function verificarEmail(email)
 {
+    //Utiliza un regex para verificar que el email cumpla con los requisitos
     let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     return emailRegex.test(email)
 }
 
 function checkTelef(tel)
 {
+    //Utiliza un regex para verificar que el telefono cumpla con los requisitos
     let telRegex = /[0-9]{3}\s[0-9]{3}\s[0-9]{4}/g;
     return telRegex.test(tel);
 
 }
 function mostrarHijos(event)
 {
+    //Cambia el display de los hijos cuando se activa la chekbox
     if(document.getElementById('hijos').checked)
     {
         document.getElementById('hijosSection').style.display = 'block';
@@ -99,6 +111,7 @@ function mostrarHijos(event)
 
 function mostrarUsuarioInfo(n)
 {
+    //Cambia el display de la informacion de un usuario cuando se apreta un boton con una id
     if(document.getElementById('us' + n).style.display == 'block')
     {
         document.getElementById('us' + n).style.display = 'none';
@@ -115,13 +128,14 @@ function verificarText(s)
     //En caso de no estar en el estandar devuelve true
     for(let i=0; i<s.length; i++)
     {
-        if(!/[a-zA-Z]/.test(s[i]) && s[i] !== 'ñ') return true;
+        if(!/[a-zA-Z]/.test(s[i]) && s[i] !== 'ñ' && s[i] !== ' ') return true;
     }
     return false;
 }
 
 function verificarNum(s)
 {
+    //Verifica que solo haya numeros en un array
     for(let i=0; i<s.length; i++)
     {
         if(/[a-zA-Z]/.test(s[i])) return true;
